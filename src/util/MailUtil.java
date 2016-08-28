0ea15ccb234193f997bdbe406fc2cbe8bf7eefa1
shiva <smachhewar@gmail.com>
2016-08-28 17:17:08 +0530

    Converted to maven project and added mail support.

---
+++ src/util/MailUtil.java
@@ -0,0 +1,70 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailUtil {
+	
+	
+	private String GMAIL_USERNAME;
+	private String GMAIL_PASSWORD;
+	
+	
+	
+	public MailUtil(String username,String password) {
+		// TODO Auto-generated constructor stub
+		GMAIL_USERNAME = username;
+		GMAIL_PASSWORD = password;
+	}
+	
+	
+	public void sendFromGMail(String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", GMAIL_USERNAME);
+        props.put("mail.smtp.password", GMAIL_PASSWORD);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(GMAIL_USERNAME));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host,GMAIL_USERNAME, GMAIL_PASSWORD);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+}
